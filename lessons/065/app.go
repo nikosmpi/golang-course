408c678945670bc7c39811363a16083d6e9a4305
Nikos Mpilalis <nikos.mpilalis@mozaik.com>
2025-11-23 15:54:24 +0200

    lesson 070

---
+++ lessons/065/app.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+	agePointer := &age
+	fmt.Println("Age:", *agePointer)
+	editAgeToAdultYears(agePointer)
+	fmt.Println("Adult years:", age)
+}
+
+func editAgeToAdultYears(age *int) {
+	*age = *age - 18
+}
---
+++ lessons/065/go.mod
@@ -0,0 +1,3 @@
+module github.com/nikosmpi/pointers
+
+go 1.25.4
