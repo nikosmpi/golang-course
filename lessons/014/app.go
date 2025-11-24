1826df831d5e5032feefcd16b1f46d5baed92a14
Nikos Mpilalis <nikos.mpilalis@mozaik.com>
2025-11-24 22:35:58 +0200

    lesson 108

--- lessons/014/app.go
+++ lessons/014/app.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	result := add("Hello World", "dadasads")
+	fmt.Println(result)
+}
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
