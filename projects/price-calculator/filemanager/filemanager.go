0f2d24400956810c1d2a385ca3a12cb6ea8831ab
Nikos Mpilalis <nikos.mpilalis@mozaik.com>
2025-12-03 19:08:40 +0200

    lesson 159 (defer)

--- projects/price-calculator/filemanager/filemanager.go
+++ projects/price-calculator/filemanager/filemanager.go
@@ -24,6 +24,7 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	var lines []string
 	for scanner.Scan() {
@@ -31,10 +32,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	}
 	err = scanner.Err()
 	if err != nil {
-		file.Close()
 		return nil, err
 	}
-	file.Close()
 	return lines, nil
 }
 
@@ -43,13 +42,12 @@ func (fm FileManager) WriteResult(data interface{}) error {
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 	time.Sleep(3 * time.Second)
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 	if err != nil {
-		file.Close()
 		return err
 	}
-	file.Close()
 	return nil
 }
